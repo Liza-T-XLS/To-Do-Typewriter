4a8abef2f196b664a892bd64cce96358f45c6cbf
Liza-T-XLS <shapi94@gmail.com>
2021-08-22 19:41:52 +0200

    Components: App + Header (basics)

---
+++ src/components/App/index.tsx
@@ -0,0 +1,21 @@
+// == Imports
+
+import React, { FC } from 'react';
+
+import './app.scss';
+
+import Header from '../Header';
+
+// == Component
+
+const App: FC = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+// == Export
+
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+// == Imports
+
+import React, { FC } from 'react';
+
+import './header.scss';
+
+// == Component
+
+const Header: FC = () => {
+  return (
+      <header className="header">
+        <h1>To-Do List</h1>
+      </header>
+  );
+}
+
+// == Export
+
+export default Header;
